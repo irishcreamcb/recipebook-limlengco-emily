8f6482f1b08da71b7ed6a51e12c88e6322194e2b
irishcreamcb <157715369+irishcreamcb@users.noreply.github.com>
2024-03-04 11:26:03 +0800

    Lab 2 commit

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+class RecipeInLine(admin.TabularInline):
+    model = Recipe
+
+class IngredientInLine(admin.TabularInline): 
+    model = Ingredient 
+
+class RecipeIngredientInLine(admin.TabularInline): 
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin): 
+    model = Recipe
+    inlines = [RecipeIngredientInLine,]
+    
+    list_display = ['name']
+
+admin.site.register(Recipe, RecipeAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model): 
+    name = models.CharField(max_length=100)
+
+    def __str__(self): 
+        return self.name
+    
+    def get_absolute_url(self): 
+        return reverse('ingredient_detail', args=[str(self.name)])
+
+class Recipe(models.Model): 
+    name = models.CharField(max_length=100)
+
+    def __str__(self): 
+        return self.name
+    
+    def get_absolute_url(self): 
+        return reverse('ledger:recipe-detail', args=[str(self.name)])
+
+class RecipeIngredient(models.Model): 
+    quantity = models.CharField(max_length=100)
+    recipe = models.ForeignKey(Recipe, 
+                               on_delete=models.CASCADE, 
+                               related_name='ingredients')
+    ingredient = models.ForeignKey(Ingredient, 
+                                   on_delete=models.CASCADE,
+                                   related_name='recipe')
+
+    def __str__(self): 
+        return '{} of {}'.format(self.quantity, self.ingredient)
